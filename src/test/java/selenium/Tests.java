10c062d1a3b2bc6f491dfc6cf2a54d15b687568a
Jonathan Carvalheiro <Jonathan.Carvalheiro@egplusww.com>
2019-05-24 21:12:21 +0100

    feat: Add Selenium tests

---
+++ src/test/java/selenium/Tests.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/java/selenium/features",
+        glue = "selenium.steps"
+)
+public class Tests {
+
+}
